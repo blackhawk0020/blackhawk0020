f3248f930d333a8ffb45aded8cb70c7ed4517be8
blackhawk0020 <94486649+blackhawk0020@users.noreply.github.com>
2021-11-16 22:38:40 +0200

    Add files via upload

---
+++ script16.js
@@ -0,0 +1,25 @@
+function Company(model, releaseDate, company, price,specs) {
+  //this = {}
+  this.model = model;
+  this.releaseDate = releaseDate;
+  this.company = company;
+  this.price = price;
+  this.specs = specs;
+  
+
+  this.getspecs = function () {
+    for (let i = 0; Company(ent))
+  }
+  
+  
+  return this
+};
+
+
+let Samsung = new Company('A50', "16.08.2015", "Samsung", 8500, { memory: '128gb', charger: "type - C", capacityBatary: "4800mA" } );
+console.log(Samsung);
+
+
+setTimeout(function() {
+  console.log(Samsung.releaseDate);
+}, 2000)
